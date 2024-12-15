bc01690d97a9b74db74f06d1829d4fb39f04d5af
Golnaz8 <golnaz.brj@gmail.com>
2024-12-15 15:59:46 -0500

    changed js code

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,7 +14,7 @@ let map = L.map("map", {
     return magnitude * 4;
   }
   
-  // Function to get marker color based on earthquake depth// I asked the hexadecimal color from chatGPT: Golnaz
+  // Function to get marker color based on earthquake depth// I asked these hexadecimal colors from chatGPT: Golnaz
   function getMarkerColor(depth) {
     if (depth > 90) {
       return "#FF4500"; 
@@ -35,9 +35,9 @@ let map = L.map("map", {
   let earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
   d3.json(earthquakeUrl).then(function(data) {
-    // Creating a GeoJSON layer with the retrieved data
+    // Creating a GeoJSON layer
     L.geoJson(data, {
-      // Style 
+      // Style code
       style: function(feature) {
         return {
           color: "#000",
@@ -55,7 +55,7 @@ let map = L.map("map", {
       // Adding explanation about that circle
       onEachFeature: function(feature, layer) {
         layer.bindPopup(
-          `<h3>${feature.properties.place}</h3><hr>
+          `<h4>${feature.properties.place}</h4>
            <p><strong>Magnitude:</strong> ${feature.properties.mag}</p>
            <p><strong>Depth:</strong> ${feature.geometry.coordinates[2]} km</p>`
         );
@@ -67,18 +67,17 @@ let map = L.map("map", {
   
     legend.onAdd = function() {
       let div = L.DomUtil.create("div", "legend");
-      let depthRanges = ["-10-10", "10-30", "30-50", "50-70", "70-90", "90+"];
+      let depths = ["-10-10", "10-30", "30-50", "50-70", "70-90", "90+"];
       let colors = ["#00FF00", "#ADFF2F", "#FFFF00", "#FFD700", "#FF8C00", "#FF4500"];
   
-      depthRanges.forEach((range, index) => {
+      for (let i = 0; i < depths.length; i++) {
+        // legend and style
         div.innerHTML += `
           <div style="display: flex; align-items: center; margin-bottom: 5px;">
-            <span style="background:${colors[index]}; width: 20px; height: 20px; display: inline-block; margin-right: 10px; border: 1px solid #000;"></span>
-            ${range}
+            <span style="background:${colors[i]}; width: 20px; height: 20px; margin-right: 10px;"></span>
+            ${depths[i]}
           </div>`;
-      });
-  
-      // style the background
+      }
       div.style.backgroundColor = "white";
       div.style.padding = "10px";
       div.style.borderRadius = "10px";
